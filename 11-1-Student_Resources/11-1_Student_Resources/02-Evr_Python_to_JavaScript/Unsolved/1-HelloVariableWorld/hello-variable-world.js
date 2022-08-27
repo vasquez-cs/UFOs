a80575d17baf37faeaf61a008dfc333bfa72af14
Carlos Vasquez <csvasquez13@gmail.com>
2022-08-26 22:20:03 -0400

    Updating files

--- 11-1-Student_Resources/11-1_Student_Resources/02-Evr_Python_to_JavaScript/Unsolved/1-HelloVariableWorld/hello-variable-world.js
+++ 11-1-Student_Resources/11-1_Student_Resources/02-Evr_Python_to_JavaScript/Unsolved/1-HelloVariableWorld/hello-variable-world.js
@@ -1,17 +1,17 @@
 // Create a variable called "fullName" that holds a string
-
+var fullname = "Homer Simpson";
 // Create a variable called "country" that holds a string
-
+var country = "Spain";
 // Create a variable called "age" that holds an integer
-
+var age = 38;
 // Create a variable called "hourlyWage" that holds an integer
-
+var hourlyWage = 22;
 // Calculate the "dailyWage" for the user
-
+var dailyWage = hourlyWage * 8;
 // Create a variable that holds a number as a string
-
+var numberAsString ="38";
 // Create a variable called 'weeklyWage' that converts a string into an integer
-
+var weeklyWage = hourlyWage * parseInt(numberAsString);
 // Create a variable called "satisfied" that holds a boolean
 
 // Print out "Hello <name>!"
